05848c340b3aa277ea1b64d056aa1d7d39c4e2cd
Othmane Zarwal <otzarwal@c1r5p3.1337.ma>
2024-10-30 21:54:39 +0100

    kkk

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: otzarwal <otzarwal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:31:24 by otzarwal          #+#    #+#             */
-/*   Updated: 2024/10/30 17:53:45 by otzarwal         ###   ########.fr       */
+/*   Updated: 2024/10/30 21:52:16 by otzarwal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,21 @@ int ft_strncmp( const char* s1, const char* s2, size_t n )
 {
     size_t i;
 
-    if (!s1 || !s2)
-        return (0);
     i = 0;
-    while (i < n && s1[i] && s2[i])
+    while (i < n  && (s1[i] || s2[i]))
     {
-        if (s1[i]!= s2[i])
-            return (s1[i] - s2[i]);
+        if ((unsigned char)s1[i] != (unsigned char)s2[i])
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
         i++;
     }
-    return (s1[i] - s2[i]);
+    return (0);
 }
 
 // int main(void)
 // {
-//     char s1[] = "Hello";
-//     char s2[] = "Hello";
+//     char s1[] = "test\200";
+//     char s2[] = "test\0";
 
-//     printf("%d\n", ft_strncmp(s1, s2, 5));
+//     printf("%d\n", ft_strncmp(s1, s2, 6));
 //     return (0);
 // }
